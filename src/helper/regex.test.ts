0199185c11326c989b1b8440b0b083bbba9ae6de
Sebastian Bejga <sebastian.bejga@iteratec.de>
2019-05-01 01:49:36 +0200

    DMS Regex

--- src/helper/regex.test.ts
+++ src/helper/regex.test.ts
@@ -1,9 +1,11 @@
-import { DM_REGEX, UTM_REGEX } from './regex';
+import { DM_REGEX, DMS_REGEX, UTM_REGEX } from './regex';
 
 describe('DM Strings', () => {
     const dms = [
         "N 36° 12.289440' E 138° 15.175440'",
-        "N 36 12.289 E 138 15.175",
+        "S 36 12.289 W 138 15.175",
+        "N 36 12.289 E 011 05.175",
+        "N 36 12.289 E 11 5.175",
     ];
     it('test strings', () => {
         dms.forEach(dm => {
@@ -16,6 +18,29 @@ describe('DM Strings', () => {
             const matchGroups = DM_REGEX.exec(dm) as RegExpExecArray;
             expect(matchGroups).not.toBeNull();
             expect(matchGroups.length).toBe(6+1);
+        });
+    })
+});
+
+describe('DMS Strings', () => {
+    const dms = [
+        "N 36° 12' 17.3664\" E 189 59' 59.9999\"",
+        "S 36 12 17.3664 W 138 15 10.5264",
+        "N 36 02 01.36 E 011 05 01.52",
+        "N 36 2 1.36 E 11 5 1.50",
+        "N 36 2 1.36 E 11 5 1.5",
+    ];
+    it('test strings', () => {
+        dms.forEach(dm => {
+            const test = DMS_REGEX.test(dm);
+            expect(test).toEqual(true);
+        });
+    });
+    it('match regex groups', () => {
+        dms.forEach(dm => {
+            const matchGroups = DMS_REGEX.exec(dm) as RegExpExecArray;
+            expect(matchGroups).not.toBeNull();
+            expect(matchGroups.length).toBe(8+1);
         });
     })
 });
